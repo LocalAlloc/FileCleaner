e887ff90033f477a79a516913eaa53329bb2bfde
LocalAlloc <72191792+LocalAlloc@users.noreply.github.com>
2023-06-05 07:43:22 -0700

    Add files via upload

---
+++ FileCleaner/pch.h
@@ -0,0 +1,16 @@
+﻿#ifndef PCH_H
+#define PCH_H
+
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <thread>
+#include <memory>
+#include <future>
+#include <mutex>
+
+#include "afxdialogex.h"
+
+#include "framework.h"
+
+#endif
